e1e6ea086f0a1bc9f4e953abc619259e11044b03
nmysolla <nmysolla@slu.edu>
2024-10-15 10:54:47 -0500

    homepage

--- registrationpage.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("registration-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-    alert("Registration successful!"); // Show success message (replace with actual submission logic)
-    this.reset(); // Clear the form after submission
-});
