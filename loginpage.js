8da1e09860de45b3130259a99afce9ff14fae45f
nmysolla <nmysolla@slu.edu>
2024-10-21 16:57:46 -0500

    linking homepage

--- loginpage.js
+++ loginpage.js
@@ -1,9 +1,23 @@
-function handleLogin(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    
-    // Simple authentication logic for demonstration
-    alert('Login successful!');
+async function handleLogin(event) {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  if (email === 'admin' && password === '12345') {
+            // Redirect to the home page after successful login
+            window.location.href = "http://127.0.0.1:5500/voting-system/homepage.html"; // Specify your home page URL here
+        } else {
+            alert('Invalid credentials. Please try again.');
+        }
+
+  try {
+      const response = await fetch(`http://127.0.0.1:8081/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`, {
+          method: 'POST',
+      });
+
+      const message = await response.text();
+      alert(message);
+  } catch (error) {
+      alert('Error: ' + error.message);
   }
-  
+}
+ src="lo"
