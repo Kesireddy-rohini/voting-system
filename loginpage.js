4aabbc2f1a07b597d06f2a9c8d818509bee14757
nmysolla <nmysolla@slu.edu>
2024-10-21 22:41:05 -0500

    updated loginpage

--- loginpage.js
+++ loginpage.js
@@ -2,22 +2,21 @@ async function handleLogin(event) {
   event.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-  if (email === 'admin' && password === '12345') {
-            // Redirect to the home page after successful login
-            window.location.href = "http://127.0.0.1:5500/voting-system/homepage.html"; // Specify your home page URL here
-        } else {
-            alert('Invalid credentials. Please try again.');
-        }
 
   try {
-      const response = await fetch(`http://127.0.0.1:8081/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`, {
-          method: 'POST',
+      const response = await fetch(`http://localhost:8081/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`, {
+          method: 'POST'
       });
 
-      const message = await response.text();
-      alert(message);
+      if (response.ok) {
+          const message = await response.text();
+          alert(message);
+          window.location.href = "homepage.html"; // Redirect to homepage
+      } else {
+          const error = await response.text();
+          alert(error);
+      }
   } catch (error) {
       alert('Error: ' + error.message);
   }
 }
- src="lo"
